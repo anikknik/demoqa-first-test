c00b052126958a93a668df160dbe2d435ccec549
anikknik <anikknik@gmail.com>
2022-04-04 10:01:44 +0300

    add modal close check

--- src/test/java/ru/evotor/crm/FirstTest.java
+++ src/test/java/ru/evotor/crm/FirstTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -72,6 +73,7 @@ public class FirstTest {
                 text(state + " " + city)
         );
         $("#closeLargeModal").click();
+        $(".modal-content").shouldNotBe(visible);
     }
 
 }
